76a0d0198470bf4131885b32116c367d4fe7a22c
meghanakonuganti <51950346+meghanakonuganti@users.noreply.github.com>
2019-08-07 12:32:42 +0530

    made TrackService trackservice as private and removed unused imports

--- src/main/java/com/stackroute/Muzixapp/controller/TrackController.java
+++ src/main/java/com/stackroute/Muzixapp/controller/TrackController.java
@@ -1,6 +1,5 @@
 package com.stackroute.Muzixapp.controller;
 import com.stackroute.Muzixapp.domain.Track;
-//import com.stackroute.Muzixapp.exceptions.TrackAlreadyExistsException;
 import com.stackroute.Muzixapp.exceptions.TrackAlreadyExistsException;
 import com.stackroute.Muzixapp.exceptions.TrackNotFoundException;
 import com.stackroute.Muzixapp.service.TrackService;
@@ -13,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "api/v2")
 public class TrackController {
-    TrackService trackService;
+   private TrackService trackService;
 
     public TrackController(TrackService trackService)
     {
