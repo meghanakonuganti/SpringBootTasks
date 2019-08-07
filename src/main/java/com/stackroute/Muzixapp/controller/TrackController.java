2d79426db03f0bcf627f2af7eab9b9aee2988cc8
meghanakonuganti <51950346+meghanakonuganti@users.noreply.github.com>
2019-08-07 12:27:06 +0530

    made TrackService service as private

--- src/main/java/com/stackroute/Muzixapp/controller/TrackController.java
+++ src/main/java/com/stackroute/Muzixapp/controller/TrackController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "api/v2")
 public class TrackController {
-    TrackService trackService;
+    private TrackService trackService;
 
     public TrackController(TrackService trackService)
     {
