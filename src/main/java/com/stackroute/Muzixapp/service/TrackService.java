03ee810b4236e495a49dec5a0978fb33ca964ef9
meghanakonuganti <51950346+meghanakonuganti@users.noreply.github.com>
2019-08-07 11:46:50 +0530

    removed unused imports and commented statements

--- src/main/java/com/stackroute/Muzixapp/service/TrackService.java
+++ src/main/java/com/stackroute/Muzixapp/service/TrackService.java
@@ -3,12 +3,11 @@ package com.stackroute.Muzixapp.service;
 import com.stackroute.Muzixapp.domain.Track;
 import com.stackroute.Muzixapp.exceptions.TrackAlreadyExistsException;
 import com.stackroute.Muzixapp.exceptions.TrackNotFoundException;
-//import com.stackroute.Muzixapp.exceptions.TrackAlreadyExistsException;
 
 import java.util.List;
 
 public interface TrackService {
-    public Track saveTrack(Track track) throws TrackAlreadyExistsException; //throws TrackAlreadyExistsException;
+    public Track saveTrack(Track track) throws TrackAlreadyExistsException; 
     public List<Track> getAllTracks();
     public Track getTrackById(int id) throws TrackNotFoundException;
     public void deleteTrack(int id);
